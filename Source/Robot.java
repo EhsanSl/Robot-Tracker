9155c8884114e171ce0d2fb244a1240b8ede4269
Ehsan <76597689+EhsanSl@users.noreply.github.com>
2022-05-17 23:31:03 -0400

    Update Robot.java

--- Source/Robot.java
+++ Source/Robot.java
@@ -1,1 +1,75 @@
+import java.awt.Point ; 
 
+/**
+ * Robot
+ */
+public class Robot {
+
+    private Point p ; 
+    private int direction  ; 
+
+    public Robot(Point p , int direction) { 
+           this.p = p ; 
+           this.direction = direction ; 
+    }
+
+    public void turnLeft(){
+        if( this.direction >= 1){
+            this.direction-- ; 
+        }
+        else { 
+            this.direction = 3 ; 
+        }
+    }
+
+    public void turnRight(){
+        if( this.direction <= 2 ){
+            this.direction++ ; 
+        }
+        else { 
+            this.direction = 0 ; 
+        }
+    }
+
+
+    public void move(){
+        if(this.direction == 0 ){
+            this.p.y-- ; 
+        } 
+        else if(this.direction == 1 ){
+            this.p.x++ ; 
+        } 
+        else if(this.direction == 2 ){
+            this.p.y++ ; 
+        } 
+        else if(this.direction == 3 ){
+            this.p.x-- ; 
+        } 
+    }
+
+
+    public Point getLocation(){
+        return this.p ; 
+    }
+
+
+    public String getDirection(){
+        if(this.direction == 0 ){
+            return "N";
+        }
+        else if(this.direction == 1){
+            return "E"; 
+        }
+        else if(this.direction == 2){
+            return "S"; 
+        }
+        else if(this.direction == 3){
+            return "W"; 
+        }
+        else{
+            return null;
+        }
+    }
+
+
+}
